5ea0aff29749bfd7cefd2e3b4663d68bb3a7cdcb
Phil <plmok61@gmail.com>
2016-12-16 16:25:47 -0600

    adds load more button

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,12 +10,12 @@ export default class App extends Component {
       businesses: [],
       page: null
     }
+    this.loadMoreBusinesses = this.loadMoreBusinesses.bind(this)
   }
 
   componentDidMount () {
     axios.get('http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses')
     .then(res => {
-      console.log(res)
       this.setState({
         businesses: res.data.businesses,
         page: 1
@@ -24,13 +24,14 @@ export default class App extends Component {
   }
 
   loadMoreBusinesses () {
-    const newPage = this.state.page + 1
+    let newPage = this.state.page + 1
+    console.log(newPage)
     axios.get(`http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses?page=${newPage}`)
     .then(res => {
-      const businesses = this.state.businesses
+      let businesses = this.state.businesses
 
-      businesses.concat(res.data.businesses)
-
+      businesses = businesses.concat(res.data.businesses)
+      console.log(businesses)
       this.setState({
         businesses: businesses,
         page: newPage
@@ -42,6 +43,7 @@ export default class App extends Component {
     return (
       <div>Hello World!
         <BusinessesList businesses={this.state.businesses} />
+        <button onClick={this.loadMoreBusinesses}>Load More</button>
       </div>
     )
   }
