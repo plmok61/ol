55f3e02b6d162e8b9120cd3e9282fa229eb8fe1d
Phil <plmok61@gmail.com>
2016-12-16 16:55:22 -0600

    adds routes to server

---
+++ server/routes/routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const path = require('path')
+
+// router.get('/', (req,res) => {
+//   res.sendFile(path.resolve(__dirname, '../../dist/index.html'))
+// })
+
+router.route('/').get((req,res) => {
+  res.sendFile(path.resolve(__dirname, '../../dist/index.html'))
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
+const router = require('./routes/routes')
 const port = 8081
 
 var http = require('http').Server(app)
@@ -15,6 +16,7 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, '../dist')))
+app.use('/', router)
 
 const server = http.listen(port)
 console.log(`Server is running on port: ${port}`)
