191360507d5f50a69893be5f29c42aa9c8745455
Phil <plmok61@gmail.com>
2016-12-16 17:14:51 -0600

    adds React Router and moves logic to get businesses into the BusinessList componenet

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,52 +1,17 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
 import BusinessesList from './BusinessesList'
 
 export default class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      businesses: [],
-      page: null
-    }
-    this.loadMoreBusinesses = this.loadMoreBusinesses.bind(this)
-  }
-
-  componentDidMount () {
-    axios.get('http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses')
-    .then(res => {
-      console.log(res.data.businesses)
-      this.setState({
-        businesses: res.data.businesses,
-        page: 1
-      })
-    })
-  }
-
-  loadMoreBusinesses () {
-    //set a new page to get businesses from
-    let newPage = this.state.page + 1
-
-    axios.get(`http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses?page=${newPage}`)
-    .then(res => {
-
-      //Concat the new businesses with the old ones
-      let businesses = this.state.businesses.concat(res.data.businesses)
-
-      //Set the updated list to state
-      this.setState({
-        businesses: businesses,
-        page: newPage
-      })
-    })
-  }
+  
 
   render () {
     return (
-      <div>Hello World!
-        <BusinessesList businesses={this.state.businesses} />
-        <button onClick={this.loadMoreBusinesses}>Load More</button>
+      <div>
+        <Router history={hashHistory}>
+          <Route path='/' component={BusinessesList} />
+        </Router>
       </div>
     )
   }
--- client/src/components/BusinessesList.js
+++ client/src/components/BusinessesList.js
@@ -1,17 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
 import BusinessListItem from './BusinessListItem'
+import axios from 'axios'
 
-const BusinessesList = ({ businesses }) => (
-  <div>
-    {
-      businesses.map((business, key) => (
-        <BusinessListItem
-          key={key}
-          business={business}
-        />
-      ))
+export default class BusinessesList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      businesses: [],
+      page: null
     }
-  </div>
-)
+    this.loadMoreBusinesses = this.loadMoreBusinesses.bind(this)
+  }
 
-export default BusinessesList
+  componentDidMount () {
+    axios.get('http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses')
+    .then(res => {
+      console.log(res.data.businesses)
+      this.setState({
+        businesses: res.data.businesses,
+        page: 1
+      })
+    })
+    .catch(err => {
+      console.log('Error getting businesses: ',err)
+    })
+  }
+
+  loadMoreBusinesses () {
+    //set a new page to get businesses from
+    let newPage = this.state.page + 1
+
+    axios.get(`http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses?page=${newPage}`)
+    .then(res => {
+
+      //Concat the new businesses with the old ones
+      let businesses = this.state.businesses.concat(res.data.businesses)
+
+      //Set the updated list to state
+      this.setState({
+        businesses: businesses,
+        page: newPage
+      })
+    })
+    .catch(err => {
+      console.log('Error getting businesses: ',err)
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        {
+          this.state.businesses.map((business, key) => (
+            <BusinessListItem
+              key={key}
+              business={business}
+            />
+          ))
+        }
+        <button onClick={this.loadMoreBusinesses}>Load More</button>
+      </div>
+    )
+  }
+}
