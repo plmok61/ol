35281c568c71f167653462cc75cac7c589d3bb11
Phil <plmok61@gmail.com>
2016-12-16 16:05:37 -0600

    gets first page of businesses on page load and sets them to state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      businesses: [],
+      page: null
+    }
+  }
+
+  componentDidMount () {
+    axios.get('http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses')
+    .then(res => {
+      console.log(res)
+      this.setState({
+        businesses: res.data.businesses,
+        page: 1
+      })
+    })
+  }
+
   render () {
     return (
       <div>Hello World!</div>
